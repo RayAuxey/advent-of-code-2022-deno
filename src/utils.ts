0b3a99fc12e725a6919c65c0fdabacb38976c4a4
RayAuxey <rayauxey@gmail.com>
2023-07-02 11:52:04 +0300

    simplify utils (read file)

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
-// Sources:
-
 export async function splitFileToLines(filepath: string) {
-  const input = await Deno.open(filepath, { read: true });
+  // const input = await Deno.open(filepath, { read: true });
 
-  let inputText = "";
-  const decoder = new TextDecoder();
-  for await (const chunk of input.readable) {
-    inputText += decoder.decode(chunk);
-  }
+  // let inputText = "";
+  // const decoder = new TextDecoder();
+  // for await (const chunk of input.readable) {
+  //   inputText += decoder.decode(chunk);
+  // }
+
+  const inputText = await Deno.readTextFile(filepath);
 
   return inputText.split("\n");
 }
