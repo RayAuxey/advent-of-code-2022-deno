c7843f19ab74f31cce3b5f2c49b4c38b21bb25ea
RayAuxey <rayauxey@gmail.com>
2023-07-02 21:55:23 +0300

    Day 2: Tests imports

---
+++ src/day2/tests.ts
@@ -0,0 +1,7 @@
+import {
+  assertEquals,
+  assert,
+} from "https://deno.land/std@0.192.0/testing/asserts.ts";
+import { splitFileToLines } from "../utils.ts";
+import { mapOutcomeToRoundScore, shapeToChoose } from "./part2.ts";
+
