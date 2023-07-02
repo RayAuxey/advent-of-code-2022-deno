2b6771ed04ec80d1967c201f00f80b5f683ce994
RayAuxey <rayauxey@gmail.com>
2023-07-02 21:42:29 +0300

    Passed Day 2 Part 1

--- src/day2/part1.ts
+++ src/day2/part1.ts
@@ -1,52 +1,18 @@
-// A,B,C are the opponent's choices
-// X,Y,Z are the player's choices
-// A=ROCK, B=PAPER, C=SCISSORS
-// X=ROCK, Y=PAPER, Z=SCISSORS
+import { splitFileToLines } from "../utils.ts";
+import { outcomeScore } from "./shared.ts";
 
-const shapeScore = {
-  X: 1,
-  Y: 2,
-  Z: 3,
-} as const;
+const lines = await splitFileToLines("./src/day2/input.txt");
 
-const roundScore = {
-  loss: 0,
-  draw: 3,
-  win: 6,
-} as const;
+console.log(calculateTotalScore(lines));
 
-type OutcomeScore = {
-  [key: string]: number;
-};
+export function calculateTotalScore(lines: string[]) {
+  let totalScore = 0;
 
-const outcomeScore: OutcomeScore = {
-  // Opponent Selects Rock
-  "A X": shapeScore.X + roundScore.draw, // Rock vs Rock
-  "A Y": shapeScore.Y + roundScore.win, // Rock vs Paper
-  "A Z": shapeScore.Z + roundScore.loss, // Rock vs Scissors
+  for (const line of lines) {
+    if (line === "") continue;
+    const [opponent, player] = line.split(" ");
+    totalScore += outcomeScore[`${opponent} ${player}`];
+  }
 
-  // Opponent Selects Paper
-  "B X": shapeScore.X + roundScore.loss, // Paper vs Rock
-  "B Y": shapeScore.Y + roundScore.draw, // Paper vs Paper
-  "B Z": shapeScore.Z + roundScore.win, // Paper vs Scissors
-
-  // Opponent Selects Scissors
-  "C X": shapeScore.X + roundScore.win, // Scissors vs Rock
-  "C Y": shapeScore.Y + roundScore.loss, // Scissors vs Paper
-  "C Z": shapeScore.Z + roundScore.draw, // Scissors vs Scissors
-};
-
-const input = await Deno.readTextFile("./src/day2/input.txt");
-const lines = input.split("\n");
-
-let totalScore = 0;
-
-for (const line of lines) {
-  if (line === "") continue;
-  const [opponent, player] = line.split(" ");
-  totalScore += outcomeScore[`${opponent} ${player}`];
+  return totalScore;
 }
-
-export { totalScore as answer };
-
-console.log(totalScore);
